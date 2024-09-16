89da238b132c361b5a5bd88d13dcca39465ef9a6
Alexander Melekhin <amelekhin96@gmail.com>
2024-09-16 18:24:25 +0300

    add permanent settings storage

--- docker/run_zed2i_node.sh
+++ docker/run_zed2i_node.sh
@@ -8,6 +8,7 @@ docker run -it --rm \
     -e DISPLAY \
     -v /dev:/dev \
     -v /tmp/.X11-unix/:/tmp/.X11-unix \
+    -v ${HOME}/zed_settings/:/usr/local/zed/settings/ \
     -v ${HOME}/zed_docker_ai/:/usr/local/zed/resources/ \
     zed_ros2_desktop_image:latest \
     ros2 launch zed_wrapper zed_multi_camera.launch.py cam_names:='[cam1,cam2]' cam_models:='[zed2i,zed2i]' cam_serials:='[33890767,30006989]'
